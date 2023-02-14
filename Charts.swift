3f48dfdb9ebd57245e7bf9097f275216cea35223
Jacqueline Hopkins <318jlh@gmail.com>
2023-02-13 19:44:16 -0800

    Update

--- Charts.swift
+++ Charts.swift
@@ -9,33 +9,24 @@ import SwiftUI
 import Charts
 
 
-//struct Recommendation: Identifiable {
-//    var nutrient: String
-//    var count: Double
-//    var id = UUID ()
-//}
-//
-//var data: [Recommendation] = [
-//    .init(nutrient: "Calcium", count: 1000),
-//    .init(nutrient: "Vitamin B12", count: 2.4),
-//    .init(nutrient: "Vitamin C", count: 75),
-//    .init(nutrient: "Iron", count: 18),
-//    .init(nutrient: "Magnesium", count: 310),
-//    .init(nutrient: "Niacon", count: 14),
-//    .init(nutrient: "Potassium", count: 260),
-//    .init(nutrient: "Sodium", count: 1500)
-//    ]
+struct Recommendation: Identifiable {
+    var nutrient: String
+    var count: Double
+    var id = UUID ()
+}
+
+var data: [Recommendation] = [
+    .init(nutrient: "Calcium", count: 1000),
+    .init(nutrient: "Vitamin B12", count: 2.4),
+    .init(nutrient: "Vitamin C", count: 75),
+    .init(nutrient: "Iron", count: 18),
+    .init(nutrient: "Magnesium", count: 310),
+    .init(nutrient: "Niacon", count: 14),
+    .init(nutrient: "Potassium", count: 260),
+    .init(nutrient: "Sodium", count: 1500)
+    ]
 
 struct Charts: View {
-    @ObservedObject var model = ViewModel()
-    struct Recommendation: Identifiable {
-        var nutrient: String
-        var count: Double
-        var id = UUID ()
-    }
-
-    var data: [Recommendation] = [
-        .init(nutrient: "Calcium", count: 1000)
     //    let comparisonValues = [
     //        Recommendation(amount: 1000, nutrientType: .calcium(.female)),
     //        Recommendation(amount: 2.4, nutrientType: .vitaminb12(.female)),
@@ -45,7 +36,7 @@ struct Charts: View {
     //        Recommendation(amount: 14, nutrientType: .niacin(.female)),
     //        Recommendation(amount: 260, nutrientType: .potassium(.female)),
     //        Recommendation(amount: 1500, nutrientType: .sodium(.female))
-            ]
+    //    ]
     var body: some View {
         Chart {
             ForEach(data) { shape in
